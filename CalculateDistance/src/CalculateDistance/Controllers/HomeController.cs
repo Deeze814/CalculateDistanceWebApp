145ed59c87068554b9036ef10e9041fd6b25ef03
Deeze814 <Deeze814@gmail.com>
2017-02-05 13:56:41 -0600

    Add initial build out of project, take info and submit data.

---
+++ CalculateDistance/src/CalculateDistance/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculateDistance.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculateDistance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Geocode(Address address)
+        {
+
+            return View("DefaultIndex");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ CalculateDistance/src/CalculateDistance/Model/Address.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CalculateDistance.Model
+{
+    public class Address
+    {
+        private string streetNumber;
+        private string streetName;
+        private string streetSuffix;
+        private string city;
+        private string state;
+        private string zip;
+        private string latitude;
+        private string longitude;
+
+        public string StreetNumber
+        {
+            get { return streetNumber; }
+            set { streetNumber = value; }
+        }
+
+        public string StreetName
+        {
+            get { return streetName; }
+            set { streetName = value; }
+        }
+
+        public string StreetSuffix
+        {
+            get { return streetSuffix; }
+            set { streetSuffix = value; }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        public string Latitude
+        {
+            get { return latitude; }
+            set { latitude = value; }
+        }
+
+        public string Longitude
+        {
+            get { return longitude; }
+            set { longitude = value; }
+        }
+    }
+}
